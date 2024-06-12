8f7be49508cd1cbdd2aa9c8e920d2d68fd6cfaef
Kinfey <kinfeylo@microsoft.com>
2024-06-13 07:20:38 +0800

    Update lab sample

--- code/07.Lab/01/extensions/phi3ext/src/extension.ts
+++ code/07.Lab/01/extensions/phi3ext/src/extension.ts
@@ -29,7 +29,7 @@ export function activate(extcontext: vscode.ExtensionContext) {
 			const code = result;
 			
 			
-			stream.progress("```txt"+"\n"+code+"```")
+			stream.progress("```txt"+"\n"+code+ +"\n"+"```")
 
 
             return { metadata: { command: 'gen' } };
@@ -39,7 +39,31 @@ export function activate(extcontext: vscode.ExtensionContext) {
 
 			const content = "Welcome to Phi-3 vision to gen code in image";
 
-			stream.progress(content);
+			const prompt = request.prompt;
+
+			if(prompt.indexOf("(IMG_URL:")>-1){
+
+				const img_url = prompt.split("(IMG_URL:")[1].split(")")[0];
+				const question = prompt.split("(IMG_URL:")[0];
+				const tmp = question;
+
+				const result = await genImage(question,img_url);
+	
+				const code = result;
+				
+				
+				stream.progress(code)
+	
+	
+				return { metadata: { command: 'img' } };
+			}
+			else
+			{
+				var result = "```txt"+"\n\n"+"Please ask question like this Your question (IMG_URL:https://example.com/image.jpg)"+ "\n\n" +"```";
+				stream.progress(result);
+
+			}
+
 
 
 
@@ -78,6 +102,10 @@ interface GenCode {
 	question: string;
 }
 
+interface ImgGenCodeResponse {
+	output: string;
+}
+
 
 interface GenCodeResponse {
 	answer: string;
@@ -96,6 +124,47 @@ async function gen(prompt: string) {
 	const post = await response.json();
 	const resultResponse = post as GenCodeResponse;
 	return resultResponse.answer;
+}
+
+async function genImage(prompt: string, img_url: string) {
+
+	// const postData: GenCode = {
+	// 	question: prompt
+	// };
+	const response = await fetch('Your Phi-3-Vision Endpoint', {
+		method: 'POST',
+		body: JSON.stringify({
+			"input_data":{
+			  "input_string":[
+				{
+				  "role":"user",
+				  "content":[ 
+					{
+					  "type": "text",
+					  "text": prompt
+					},
+					{
+						"type": "image_url",
+						"image_url": {
+						  "url": img_url
+						}
+					}
+				  ]
+				}
+			  ],
+			  "parameters":{
+					"temperature": 0.6,
+					"top_p": 0.9,
+					"do_sample": false,
+					"max_new_tokens": 2048
+			  }
+			}
+		}),
+		headers: { 'Content-Type': 'application/json', 'Authorization' : 'Bearer Your Phi-3-Vision Endpoint Key', 'azureml-model-deployment': 'Your Phi-3-Vision Deployment name' }
+	});
+	const post = await response.json();
+	const resultResponse = post as ImgGenCodeResponse;
+	return resultResponse.output;
 }
 
 // This method is called when your extension is deactivated
