578f2398e9430baaaba972e21495427214a6f9f7
Bruno Capuano <bcapuano@gmail.com>
2025-01-27 11:35:14 -0500

    Refactor for Ollama integration and UI enhancements
    Updated devcontainer configuration to pull models and improved formatting.
    Refactored Program.cs to replace OpenAI with Ollama chat completion and enhanced chat history management.
    Updated project files to reference newer Semantic Kernel packages and added memory management components.
    Introduced SpectreConsoleOutput.cs for better console message formatting and improved overall code structure for readability.

--- md/07.Labs/CsharpOllamaCodeSpaces/src/Sample03/Program.cs
+++ md/07.Labs/CsharpOllamaCodeSpaces/src/Sample03/Program.cs
@@ -23,87 +23,60 @@
 //    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //    THE SOFTWARE.
 
-#pragma warning disable SKEXP0001
-#pragma warning disable SKEXP0003
-#pragma warning disable SKEXP0010
-#pragma warning disable SKEXP0011
-#pragma warning disable SKEXP0050
-#pragma warning disable SKEXP0052
+#pragma warning disable SKEXP0001, SKEXP0003, SKEXP0010, SKEXP0011, SKEXP0050, SKEXP0052, SKEXP0070
 
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.KernelMemory;
+using Microsoft.KernelMemory.AI.Ollama;
 using Microsoft.SemanticKernel;
-using Microsoft.SemanticKernel.Connectors.OpenAI;
-using Microsoft.SemanticKernel.Embeddings;
-using Microsoft.SemanticKernel.Memory;
-using Microsoft.SemanticKernel.Plugins.Memory;
 
+var ollamaEndpoint = "http://localhost:11434";
+var modelIdChat = "phi3.5";
+var modelIdEmbeddings = "all-minilm";
+
+// questions
 var question = "What is Bruno's favourite super hero?";
+
 Console.WriteLine($"This program will answer the following question: {question}");
 Console.WriteLine("1st approach will be to ask the question directly to the Phi-3 model.");
 Console.WriteLine("2nd approach will be to add facts to a semantic memory and ask the question again");
 Console.WriteLine("");
 
 // Create a chat completion service
-var builder = Kernel.CreateBuilder();
-builder.AddOpenAIChatCompletion(
-    modelId: "phi3.5",
-    endpoint: new Uri("http://localhost:11434"),
-    apiKey: "apikey");
-builder.AddLocalTextEmbeddingGeneration();
-Kernel kernel = builder.Build();
-
-Console.WriteLine($"Phi-3 response (no memory).");
+Kernel kernel = Kernel.CreateBuilder()
+    .AddOllamaChatCompletion(modelId: modelIdChat, endpoint: new Uri(ollamaEndpoint))
+    .Build();
 var response = kernel.InvokePromptStreamingAsync(question);
 await foreach (var result in response)
 {
-    Console.Write(result);
+    Console.Write(result.ToString());
 }
 
 // separator
 Console.WriteLine("");
 Console.WriteLine("==============");
 Console.WriteLine("");
-
-// get the embeddings generator service
-var embeddingGenerator = kernel.Services.GetRequiredService<ITextEmbeddingGenerationService>();
-var memory = new SemanticTextMemory(new VolatileMemoryStore(), embeddingGenerator);
-
-// add facts to the collection
-const string MemoryCollectionName = "fanFacts";
-
-await memory.SaveInformationAsync(MemoryCollectionName, id: "info1", text: "Gisela's favourite super hero is Batman");
-await memory.SaveInformationAsync(MemoryCollectionName, id: "info2", text: "The last super hero movie watched by Gisela was Guardians of the Galaxy Vol 3");
-await memory.SaveInformationAsync(MemoryCollectionName, id: "info3", text: "Bruno's favourite super hero is Invincible");
-await memory.SaveInformationAsync(MemoryCollectionName, id: "info4", text: "The last super hero movie watched by Bruno was Aquaman II");
-await memory.SaveInformationAsync(MemoryCollectionName, id: "info5", text: "Bruno don't like the super hero movie: Eternals");
-
-TextMemoryPlugin memoryPlugin = new(memory);
-
-// Import the text memory plugin into the Kernel.
-kernel.ImportPluginFromObject(memoryPlugin);
-
-OpenAIPromptExecutionSettings settings = new()
-{
-    ToolCallBehavior = null,
-};
-
-
-var prompt = @"
-    Question: {{$input}}
-    Answer the question using the memory content: {{Recall}}";
-
-var arguments = new KernelArguments(settings)
-{
-    { "input", question },
-    { "collection", MemoryCollectionName }
-};
-
 Console.WriteLine($"Phi-3 response (using semantic memory).");
 
-response = kernel.InvokePromptStreamingAsync(prompt, arguments);
-await foreach (var result in response)
+var configOllamaKernelMemory = new OllamaConfig
 {
-    Console.Write(result);
-}
+    Endpoint = ollamaEndpoint,
+    TextModel = new OllamaModelConfig(modelIdChat),
+    EmbeddingModel = new OllamaModelConfig(modelIdEmbeddings, 2048)
+};
 
-Console.WriteLine($"");
+var memory = new KernelMemoryBuilder()
+    .WithOllamaTextGeneration(configOllamaKernelMemory)
+    .WithOllamaTextEmbeddingGeneration(configOllamaKernelMemory)
+    .Build();
+
+await memory.ImportTextAsync("Gisela's favourite super hero is Batman");
+await memory.ImportTextAsync("The last super hero movie watched by Gisela was Guardians of the Galaxy Vol 3");
+await memory.ImportTextAsync("Bruno's favourite super hero is Invincible");
+await memory.ImportTextAsync("The last super hero movie watched by Bruno was Deadpool and Wolverine");
+await memory.ImportTextAsync("Bruno don't like the super hero movie: Eternals");
+
+var answer = memory.AskStreamingAsync(question);
+await foreach (var result in answer)
+{
+    Console.Write(result.ToString());
+}
