9423434c5f1e49a48884bb5bd083e77792d9e8a3
Bruno Capuano <bcapuano@gmail.com>
2025-02-27 12:28:09 -0500

    C# samples to demonstrate the integration of Phi-3 and Phi-4 models in a .NET app using ONNX
    This pull request introduces a new set of C# labs to demonstrate the integration of Phi-3 and Phi-4 models in a .NET environment. The changes include the addition of project files, solution configuration, and sample code for various labs.
    
    New Labs and Projects:
    md/04.HOL/dotnet/csharplabs.md: Added documentation for the Phi-3 labs using C#, including prerequisites, instructions for downloading models, and descriptions of the sample projects.
    
    md/04.HOL/dotnet/src/LabsPhi.sln: Created a new Visual Studio solution file that includes multiple projects for the labs. The solution file organizes the Phi-3 and Phi-4 projects, including LabsPhi301, LabsPhi302, LabsPhi303, LabsPhi304, and chat projects LabsPhi4-Chat-01OnnxRuntime and `LabsPhi4-Chat-02SK. These changes provide a comprehensive set of labs to showcase the capabilities of Phi-3 and Phi-4 models in various scenarios using C#

---
+++ md/04.HOL/dotnet/src/LabsPhi4-Chat-01OnnxRuntime/Program.cs
@@ -0,0 +1,72 @@
+﻿//    Copyright (c) 2025
+//    Author      : Bruno Capuano
+//    Change Log  :
+//
+//    The MIT License (MIT)
+//
+//    Permission is hereby granted, free of charge, to any person obtaining a copy
+//    of this software and associated documentation files (the "Software"), to deal
+//    in the Software without restriction, including without limitation the rights
+//    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the Software is
+//    furnished to do so, subject to the following conditions:
+//
+//    The above copyright notice and this permission notice shall be included in
+//    all copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//    THE SOFTWARE.
+
+using Microsoft.ML.OnnxRuntimeGenAI;
+
+// Phi4
+var modelPath = @"d:\phi\models\Phi-4-mini-instruct-onnx\cpu_and_mobile\cpu-int4-rtn-block-32-acc-level-4\";
+
+var model = new Model(modelPath);
+var tokenizer = new Tokenizer(model);
+
+var systemPrompt = "You are an AI assistant that helps people find information. Answer questions using a direct style. Do not share more information that the requested by the users.";
+
+// chat start
+Console.WriteLine(@"Ask your question. Type an empty string to Exit.");
+
+// chat loop
+while (true)
+{
+    // Get user question
+    Console.WriteLine();
+    Console.Write(@"Q: ");
+    var userQ = Console.ReadLine();
+    if (string.IsNullOrEmpty(userQ))
+    {
+        Console.WriteLine("Bye!");
+        break;
+    }
+
+    // show phi response
+    Console.Write("Phi4: ");
+    var fullPrompt = $"<|system|>{systemPrompt}<|end|><|user|>{userQ}<|end|><|assistant|>";
+    var tokens = tokenizer.Encode(fullPrompt);
+
+    var generatorParams = new GeneratorParams(model);
+    generatorParams.SetSearchOption("max_length", 2048);
+    generatorParams.SetSearchOption("past_present_share_buffer", false);
+
+    var generator = new Generator(model, generatorParams);
+    generator.AppendTokens(tokens[0].ToArray());
+    while (!generator.IsDone())
+    {
+        //generator.ComputeLogits();
+        generator.GenerateNextToken();
+        var outputTokens = generator.GetSequence(0);
+        var newToken = outputTokens.Slice(outputTokens.Length - 1, 1);
+        var output = tokenizer.Decode(newToken);
+        Console.Write(output);
+    }
+    Console.WriteLine();
+}
